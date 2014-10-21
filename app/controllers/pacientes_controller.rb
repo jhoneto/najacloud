67fc85ad50c348040af67bc113c358cb28adaf81
Vinícius Oyama <vinicius.oyama@codus.com.br>
2014-10-21 15:43:43 -0200

    configured simple form

--- app/controllers/pacientes_controller.rb
+++ app/controllers/pacientes_controller.rb
@@ -2,16 +2,20 @@ class PacientesController < InheritedResources::Base
   has_scope :por_organizacao
   has_scope :por_nome
   actions :all, except: [:show]
-  
+
 
   def index
     @pacientes = []
   end
-  
+
+  def new
+    @pacient = Paciente.new
+  end
+
   def listar
     collection
   end
-  
+
   def update
     update! do |format|
       format.html { (params[:acao] == 'salvar_e_novo') ? redirect_to(new_paciente_path) : redirect_to(pacientes_path) }
